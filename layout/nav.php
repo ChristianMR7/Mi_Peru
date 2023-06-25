0570ce9af0959fcc2f98d98f48376433dd5fce27
ChristianMR7 <christianrq77@gmail.com>
2023-06-25 17:34:00 -0400

    cambio a index

--- layout/nav.php
+++ layout/nav.php
@@ -6,7 +6,7 @@
 </style>
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark text-white"> 
   <div class="container-fluid">
-      <strong><a class="navbar-brand fw-bold" href="MiPeru.php">MIPERU</a></strong>
+      <strong><a class="navbar-brand fw-bold" href="index.php">MIPERU</a></strong>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
